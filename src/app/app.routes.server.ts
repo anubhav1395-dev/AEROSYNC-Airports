5a184a16fd2ec5c71873e7614935ba727fd82ee0
Anubhav Biswas <anubhavbiswas02@gmail.com>
2025-12-26 15:42:57 +0530

    sign up 4

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,10 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'deal-details/:id',
-    renderMode: RenderMode.Server // This fixes the "getPrerenderParams" error
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
